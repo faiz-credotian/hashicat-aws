4108f9b458b2fca4d3984be4ff53a37bd68e29bd
FaizSoftTeche <79360188+faiz-credotian@users.noreply.github.com>
2021-10-07 17:55:14 +0530

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${FAIZKHAN}'s app. Hurray! The Time has Come, Launching our new Product.
   <!-- END -->
 
   </div>
